0f2debd17a2586f30f32c327bbf57e4bd2f11b51
imvishalnag <imvishalnag@gmail.com>
2019-11-30 12:36:08 +0530

    update

---
+++ resources/views/admin/reciet.blade.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      <!-- Basic page needs -->
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Bibibobi</title>
+  <!-- Mobile specific metas  -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Favicons Icon -->
+  <link rel="shortcut icon" type="image/x-icon" href="images/fab.png">
+  <!-- CSS Style -->
+  <link rel="stylesheet" href="{{asset('web/css/url.css')}}">
+
+    <style>img{width: 100%;margin: 10px auto!important;}.flot-r{float: right;}.flot-l{float: left;}.width-30{width:30%}.row{margin: 0;border-bottom: 1px solid #333}.bdr-botm{border-bottom:none}.bdr-r{border-right: 1px solid #333;}.bdr-l{border-left: 1px solid #333;}.main-content{padding: 10px 0}.content-body{border:1px solid #333;background-color: #fff}.font-15{font-size: 15px}.cap{text-transform: uppercase;}p,h3{margin: 5px 0;}.ptb-30{padding: 30px 0}.bar{display: flex;margin: auto;}p{color: #333;}</style>
+  </head>
+  <body>
+      <!-- Main Content -->
+      <section class="main-content">
+        <div class="container">
+          <div class="row bdr-botm">
+            <div class="col-md-3 col-xs-2"></div>
+            <div class="col-md-6 col-xs-8">
+              <div class="content-body">
+                <div class="row">
+                  <div class="col-xs-6"> <h4 style="margin-top:14px">BIBIBOBI SURFACE</h4></div>
+                  <div class="col-xs-6 bdr-l"><img src="{{asset('images/receipt/1.jpg')}}"></div>
+                </div> 
+                <div class="row">
+                  <div class="col-xs-12"><img class="width-30 bar" src="{{asset('images/receipt/barcode.jpg')}}"></div>
+                  <div class="col-xs-6"> <p class="font-15 flot-l">1214585268</p></div>
+                  <div class="col-xs-6"> <p class="font-15 flot-r"><strong>SLG/GHY</strong></p></div>
+                </div>
+                <div class="row">
+                  <div class="col-xs-9">
+                    <h5>Shiping Address :</h5>
+                    <h4 class="cap">GULZAR CHOUDHURY</h4>
+                    <p>HKD<br>
+                    <p>Lalapur_Ward1_D (Assam)<br>
+                    <p><strong>PIN:788161</strong></p>
+                  </div>
+                  <div class="col-xs-3 bdr-l" style="height: 120px;"> <h4 style="position: absolute;top: 45px;left: 32px;" class="font-15 flot-r"><strong class="cap">Prepaid</strong></h4></div>
+                </div>
+                <div class="row">
+                  <div class="col-xs-7 bdr-r">
+                    <p class="cap"><strong>Seller:</strong> BIBIBOBI MARKET SERVICES (OPC) PVT LTD</p>
+                    <p><strong>Address:</strong> House No. 453, Halwa Gaon, Dhekial, Golaghat, Golaghat, Assam - 785621</p>
+                  </div>
+                  <div class="col-xs-5"></div>
+                </div> 
+                <div class="row">
+                  <div class="col-xs-7 bdr-r">
+                    <p class="cap"><strong>Product</strong></p>
+                  </div>
+                  <div class="col-xs-5">
+                    <div class="row bdr-botm">
+                      <div class="col-xs-6 bdr-r"><p class="cap"><strong>Price</strong></p></div>
+                      <div class="col-xs-6"><p class="cap"><strong>Total</strong></p></div>
+                    </div>  
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-xs-7 bdr-r">
+                    <p class="cap ptb-30"><strong>Roaster White Strip Shirt Half</strong></p>
+                  </div>
+                  <div class="col-xs-5">
+                    <div class="row bdr-botm">
+                      <div class="col-xs-6 bdr-r"><p class="cap ptb-30"><strong>₹ 899</strong></p></div>
+                      <div class="col-xs-6"><p class="cap ptb-30"><strong>₹ 899</strong></p></div>
+                    </div>  
+                  </div>
+                </div> 
+                <div class="row">
+                  <div class="col-xs-7 bdr-r">
+                    <p class="cap"><strong>Total</strong></p>
+                  </div>
+                  <div class="col-xs-5">
+                    <div class="row bdr-botm">
+                      <div class="col-xs-6 bdr-r"><p class="cap"><strong>₹ 899</strong></p></div>
+                      <div class="col-xs-6"><p class="cap"><strong>₹ 899</strong></p></div>
+                    </div>  
+                  </div>
+                </div> 
+                <div class="row">
+                  <div class="col-xs-12"><img class="width-30 bar" src="{{asset('images/receipt/barcode.jpg')}}"></div>
+                </div>
+                <div class="row bdr-botm">
+                  <div class="col-xs-12"><p><strong>Return Address:</strong> Hkd,Guwahati,Assam - Lalapur - 788163</p></div>
+                </div>   
+              </div>
+            </div>
+            <div class="col-md-3 col-xs-2"></div>
+          </div>        
+        </div>
+      </section>
+      <!-- Main Content -->
+
+  
+  </body>
+</html>
+
