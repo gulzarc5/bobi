be44ed263bd079c9a8c313b1ce6b4eb56704b0aa
gulzarc5 <gulzarc5@gmail.com>
2020-02-04 16:48:47 +0530

    forgot pass

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -567,7 +567,6 @@ class UsersController extends Controller
         $validator =  Validator::make($request->all(),[
             'otp' => 'required',
             'mobile' => ['required', 'numeric', 'digits:10'],
-            'current_pass' => ['required', 'string', 'min:8'],
             'new_password' => ['required', 'string', 'min:8', 'same:confirm_password'],
         ]);
 
